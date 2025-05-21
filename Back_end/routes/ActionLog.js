fd778dd9360959ff1cb88975c45ea1c6fb956a1f
Phạm Huy Hoàng <105590908+Hoang25092003@users.noreply.github.com>
2025-05-21 22:57:40 +0700

    Create ActionLog.js

---
+++ Back_end/routes/ActionLog.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { getPool } = require('../config/db');
+const { authenticateToken } = require('../middleware/auth');
+
+router.get('/actionLog', authenticateToken, async (req, res) => {
+    try {
+        const pool = getPool();
+        const result = await pool.request().query(`
+          SELECT * FROM ActionLog ORDER BY action_time
+        `);
+        res.json(result.recordset);
+    } catch (error) {
+        console.log("Lỗi khi lấy log:", error);
+        res.status(500).json({ error: "Lỗi server khi lấy log" });
+    }
+});
+
+
+module.exports = router;
